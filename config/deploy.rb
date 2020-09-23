345ea8a414baba48a3ff3ddea19f9c4462bac38d
Naozumi Numata <naozumi225@gmail.com>
2020-09-24 01:28:38 +0900

    Add for deploy by capistrano 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 
 # rubyのバージョン
+set :rbenv_type, :user
 set :rbenv_ruby, '2.6.6'
+set :rbenv_custom_path, '/root/.rbenv'
 
 #出力するログのレベル。
 set :log_level, :debug
