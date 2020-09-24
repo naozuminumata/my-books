c3b0458de2fbe6d8796668aba1a07089b2b7b264
Naozumi Numata <naozumi225@gmail.com>
2020-09-24 22:08:45 +0900

    Add for deploy by capistrano 10

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ server '54.144.26.150', user: 'naozumi', roles: %w{app db web}
 
 set :ssh_options, {
   port: 22, #XXXにポート番号
-  keys: [File.expand_path('~/.ssh/mybooks_key_rsa')], #'~/.ssh/id_rsa'部分をローカル環境の鍵のパスに
+  keys: [File.expand_path('~/.ssh/id_rsa')], #'~/.ssh/id_rsa'部分をローカル環境の鍵のパスに
   forward_agent: true,
   auth_methods: %w(publickey)
 }
